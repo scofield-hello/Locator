03d699d68755a84f329cb564eb407c6a6bca935a
liang.wang <scofield.hello@gmail.com>
2015-06-10 23:47:32 +0800

    新建项目

---
+++ src/com/icoding/locator/LocatorService.java
@@ -0,0 +1,32 @@
+package com.icoding.locator;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LocatorService extends Service {
+	
+	public static final String BROADCAST_LOCATION_START = "com.icoding.locator.BROADCAST_LOCATION_START";
+	public static final String BROADCAST_LOCATION_STOP = "com.icoding.locator.BROADCAST_LOCATION_STOP";
+	public static final String BROADCAST_LOCATION_ERROR = "com.icoding.locator.BROADCAST_LOCATION_ERROR";
+	
+	public static final String EXTRA_TIME = "com.icoding.locator.EXTRA_TIME";
+	public static final String EXTRA_ADDRESS = "com.icoding.locator.EXTRA_ADDRESS";
+	public static final String EXTRA_COUNTRY = "com.icoding.locator.EXTRA_COUNTRY";
+	public static final String EXTRA_LATITUDE = "com.icoding.locator.EXTRA_LATITUDE";
+	public static final String EXTRA_LONGITUDE = "com.icoding.locator.EXTRA_LONGITUDE";
+	public static final String EXTRA_BEARING = "com.icoding.locator.EXTRA_BEARING";
+	public static final String EXTRA_SPEED = "com.icoding.locator.EXTRA_SPEED";
+	public static final String EXTRA_ALTITUDE = "com.icoding.locator.EXTRA_ALTITUDE";
+	public static final String EXTRA_RADIUS = "com.icoding.locator.EXTRA_RADIUS";
+	public static final String EXTRA_ERROR = "com.icoding.locator.EXTRA_ERROR";
+	public static final String EXTRA_PROVIDER = "com.icoding.locator.EXTRA_PROVIDER";
+	public static final String EXTRA_FLOOR = "com.icoding.locator.EXTRA_FLOOR";
+	public static final String EXTRA_ACCURACY = "com.icoding.locator.EXTRA_ACCURACY";
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+}
